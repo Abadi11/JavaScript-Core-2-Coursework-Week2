9b714efba47dc83ad2ba8e2a524cb7e58c69b676
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2021-08-03 21:36:56 +0100

    Update B

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,13 @@
-function shoppingList(arrayOfPeople) {
+function shoppingList(arrayOfShopping) {
   // Write your code here...
+  let content = document.querySelector("#content");
+  let unordered = document.createElement("ul");
+  arrayOfShopping.forEach((item) => {
+    let orderedList = document.createElement("li");
+    orderedList.innerText = item;
+    unordered.appendChild(orderedList);
+    content.appendChild(unordered)
+  })
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
