54b3f263e5904549d32016e1b6b7bb8a4ecfe6ab
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2021-08-04 19:55:07 +0100

    Update script.js

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,5 +1,28 @@
 function todoList(todos) {
   // Write your code here...
+  let contentId = document.querySelector("#content");
+  let unorderedEl = document.createElement("ul");
+  contentId.appendChild(unorderedEl);
+  todos.forEach(todo => {
+    let list = document.createElement("li");
+    list.innerText = todo.todo;
+    list.addEventListener("click", lineThrough);
+    
+    function lineThrough(){
+      list.style.textDecoration = "line-through";
+      
+    };
+    list.addEventListener("click",function(){
+      document.removeEventListener("click", lineThrough);
+    });
+    
+    //list.removeEventListener("mouseover", lineThrough);
+    /*list.addEventListener("click", noLineThrough);
+    function noLineThrough(){
+      list.style.textDecoration = "none";
+    };*/
+    unorderedEl.appendChild(list)
+  });
 }
 
 const todos = [
