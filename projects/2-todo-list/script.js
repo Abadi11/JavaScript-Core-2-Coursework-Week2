a43fe9e34bbdf6911f8d6ce5eeb1f098349b3403
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2021-08-10 21:34:31 +0100

    update add new todo

--- projects/2-todo-list/script.js
+++ projects/2-todo-list/script.js
@@ -24,7 +24,25 @@ function populateTodoList(todos) {
     spanEl.appendChild(iEl2);
     liEl.appendChild(spanEl);
     list.appendChild(liEl);
-  })
+
+    // The first `<i>` tag that applies a line-through text-decoration styling
+  iEl1.addEventListener("click", function(){
+    if (liEl.style.textDecoration === "line-through"){
+      liEl.style.textDecoration = "none";
+    }else{
+      liEl.style.textDecoration = "line-through";
+    };
+  });
+
+  // The second `<i>` tag that deletes the parent `<li>` element from the `<ul>`.
+  iEl2.addEventListener("click", function (){
+      liEl.remove();
+  });
+  
+  });
+
+
+  
 
 }
 
@@ -35,15 +53,24 @@ let todos = [
   { task: "Do the shopping", completed: false },
 ];
 
-populateTodoList(todos);
+
 
 // This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
 function addNewTodo(event) {
   // The code below prevents the page from refreshing when we click the 'Add Todo' button.
   event.preventDefault();
   // Write your code here... and remember to reset the input field to be blank after creating a todo!
+  
+    const inputTodo = document.getElementById("todoInput").value; 
+    let obj = {task: inputTodo, completed: false};
+    populateTodoList([obj]);
+    todos.push(obj)
+    
+  
 }
+console.log(todos);
 
+populateTodoList(todos);
 // Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
 function deleteAllCompletedTodos() {
   // Write your code here...
