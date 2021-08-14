39383760a3bb9dfd31d6347ed56846adae26f7b7
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2021-08-14 01:32:30 +0100

    update 2-todo-list

--- projects/2-todo-list/script.js
+++ projects/2-todo-list/script.js
@@ -1,7 +1,13 @@
 function populateTodoList(todos) {
   const list = document.getElementById("todo-list");
   // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
-  todos.forEach((todo) => {
+
+  list.replaceChildren([]);
+
+  todos.map(createTodoList).forEach((liEl) => list.appendChild(liEl));
+}
+function createTodoList(todo){
+
     // create li with its stuff
     const liEl = document.createElement("li");
     liEl.innerText = todo.task;
@@ -12,24 +18,32 @@ function populateTodoList(todos) {
     spanEl.className = "badge bg-primary rounded-pill";
 
     // create i elements with their stuff
-    const iEl1 = document.createElement("i");
-    iEl1.className = "fa fa-check";
-    iEl1.setAttribute("aria-hidden", true);
-    const iEl2 = document.createElement("i");
-    iEl2.className = "fa fa-trash";
-    iEl2.setAttribute("aria-hidden", true);
+    const completedBtn = document.createElement("i");
+    completedBtn.className = "fa fa-check";
+    completedBtn.setAttribute("aria-hidden", true);
+    completedBtn.dataset.task = todo.task;
+    completedBtn.addEventListener("click", handleCompletedBtn);
+    
+    if(todo.completed){
+      liEl.style.textDecoration = "line-through";
+    }
 
+    const deletedBtn = document.createElement("i");
+    deletedBtn.className = "fa fa-trash";
+    deletedBtn.setAttribute("aria-hidden", true);
+    deletedBtn.dataset.task = todo.task;
+    deletedBtn.addEventListener("click", handleDeletedBtn);
     
 
     // append child to all elements
-    spanEl.appendChild(iEl1);
-    spanEl.appendChild(iEl2);
+    spanEl.appendChild(completedBtn);
+    spanEl.appendChild(deletedBtn);
     liEl.appendChild(spanEl);
-    list.appendChild(liEl);
+    
     
     // The first `<i>` tag that applies a line-through text-decoration styling
-  iEl1.addEventListener("click", function(){
-    todo.completed = true;
+  //completedBtn.addEventListener("click", function(){
+    //todo.completed = true;
     /*
     if (liEl.style.textDecoration === "line-through"){
       liEl.style.textDecoration = "none";
@@ -37,73 +51,86 @@ function populateTodoList(todos) {
       liEl.style.textDecoration = "line-through";
     };
     */
-    if(todo.completed){
-      liEl.style.textDecoration = "line-through";
-    } 
-  });
+    //if(todo.completed){
+      //liEl.style.textDecoration = "line-through";
+    //} 
+  //});
 
   // The second `<i>` tag that deletes the parent `<li>` element from the `<ul>`.
-  iEl2.addEventListener("click", function (){
-      liEl.remove();
-  });
-  
-  });
+  //deletedBtn.addEventListener("click", function (){
+  //    liEl.remove();
+  //});
 
-  
-
-  
-
+  return liEl;
 }
 
-// These are the same todos that currently display in the HTML
-// You will want to remove the ones in the current HTML after you have created them using JavaScript
+function handleCompletedBtn(event){
+  
+  let currentTask = event.currentTarget.dataset.task;
+  todos
+    .filter((todo) => todo.task === currentTask)
+    .forEach(toggleTodoCompleted);
+  
+  populateTodoList(todos);
+}
+
+function handleDeletedBtn (event){
+  let currentTask = event.currentTarget.dataset.task;
+  let index = todos.indexOf((todo) => todo.task === currentTask);
+  //console.log(index)
+  deleteTodo(index)
+  populateTodoList(todos);
+}
+
 let todos = [
   { task: "Wash the dishes", completed: false },
   { task: "Do the shopping", completed: false },
 ];
 
+populateTodoList(todos);
+
+
+
 
 
 // This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
 function addNewTodo(event) {
-  // The code below prevents the page from refreshing when we click the 'Add Todo' button.
   event.preventDefault();
   // Write your code here... and remember to reset the input field to be blank after creating a todo!
   
-    const inputTodo = document.getElementById("todoInput").value; 
-    let obj = {task: inputTodo, completed: false};
-    populateTodoList([obj]);
-    todos.push(obj)
-    
+    const inputTodo = document.getElementById("todoInput"); 
+    let newTodo = {task: inputTodo.value, completed: false};
+    todos.push(newTodo);
+    populateTodoList(todos);
+    inputTodo.value = "";
   
 }
-console.log(todos);
 
-populateTodoList(todos);
+function toggleTodoCompleted(todo){
+  todo.completed = !todo.completed;
+}
+
+function deleteTodo(index){
+  todos.splice(index , 1);
+}
+
+
 // Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
 
 // create a delete completed button in the end of list
 
-function deleteAllCompletedTodos() {
+function deleteAllCompletedTodos(event) {
   // Write your code here...
-  const contentOfList = document.querySelector(".col-sm");
-  const deleteCompletedBtn = document.createElement("button");
-  deleteCompletedBtn.innerText = "Delete Completed Task";
-  deleteCompletedBtn.className = "btn btn-primary mb-3";
-  contentOfList.appendChild(deleteCompletedBtn)
-
-  //
-
-  
-
-  deleteCompletedBtn.addEventListener("click", () => {
-    list = document.querySelectorAll("li .list-group-item")
-    list.forEach((li) => {
+  event.preventDefault();
+  /*
+    let allLi = document.querySelectorAll("#todo-list li");
+    allLi.forEach((li) => {
       if (li.style.textDecoration === "line-through"){
-      li.delete();
-    }
+        li.remove();
+      }
     })
-    
-  })
+    */
+  todos = todos.filter((todo) => !todo.completed);
+  populateTodoList(todos);
 }
-deleteAllCompletedTodos()
+console.log(todos);
