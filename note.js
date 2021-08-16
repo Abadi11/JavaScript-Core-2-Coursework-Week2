0c1dffa88f209180eb4e6ae8838b31b7ba7e5361
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2021-08-16 10:27:42 +0100

    Update note.js

--- note.js
+++ note.js
@@ -14,15 +14,30 @@ var sing = function () {
     lyrics.push(song2[i])
   }
   */
-  let i = 0 
-  let song;
-  while (i === 2){
-   song[i] = bottels.toString() + " bottles of beer on the wall, " + bottels.toString() + " bottles of beer." ;
-    lyrics.push(song[i])
-    i++
-    
+  let i = bottels
+  while (i >= 0){
+    if (i === 99){
+      song0 = i + " bottles of beer on the wall, " + i + " bottles of beer.";
+      lyrics.push(song0);
+    }else if (i >= 1){
+      song1 = "Take one down and pass it around, " + (i)+ " bottles of beer on the wall.";
+      lyrics.push(song1)
+      song2 = (i) + " bottles of beer on the wall, " + (i) + " bottles of beer.";
+      lyrics.push(song2)
+    }else if (i === 1){
+      song1 = "Take one down and pass it around, " + (i) + " bottle of beer on the wall.";
+      lyrics.push(song1)
+      song2 = (i) + " bottle of beer on the wall, " + (i) + " bottle of beer.";
+      lyrics.push(song2)
+    }else{
+      lyrics.push("Take one down and pass it around, no more bottles of beer on the wall.", "No more bottles of beer on the wall, no more bottles of beer.", "Go to the store and buy some more, 99 bottles of beer on the wall.")
+      
+    }
+    i--;
   }
   return lyrics
 };
 
-console.log(sing())
+let a = sing()
+let b = a[196]
+console.log(b)
